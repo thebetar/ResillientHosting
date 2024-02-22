c3014e81a3e46016c3f6045eb7b498eac1148952
Lars Vonk <larserikvonk@ziggo.nl>
2024-02-22 19:10:03 +0100

    chore: add shared volume for counting

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,9 @@ const html = fs.readFileSync('src/index.html', 'utf8');
 const app = express();
 
 app.get('/', (req, res) => {
+    const counter = fs.readFileSync('data/data.txt', 'utf8');
+    fs.writeFileSync('data/data.txt', String(parseInt(counter) + 1));
+
     res.send(html.replace('{{SERVER_NAME}}', process.env.HOSTNAME || 'Server'));
 });
 
